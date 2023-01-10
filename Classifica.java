ae8de3398b9f957138a2975047042d0a56103cb2
Calogero Sidoti Pipirito <calogero.pipirito@exprivia.com>
2023-01-10 12:23:45 +0100

    Commit con Classifica modificato

--- Classifica.java
+++ Classifica.java
@@ -1,42 +1,45 @@
 package progetto1;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Classifica {
 
-    private SecureRandom n = new SecureRandom();
+    private static HashMap<String, int> classifica = new HashMap<String, int>();
     private static ArrayList<String> squadre = new ArrayList<>();
-    private static ArrayList<Integer> punti = new ArrayList<>();
 
-    public static void aggiungiSquadra(Squadra squadra)
-    {
-        for (int i = 0; i < squadre.size(); i++)
-        {
-            if(squadre.contains(squadra.getNome()))
+    private SecureRandom n = new SecureRandom();
+
+
+    public static void aggiungiSquadraConPunti(Squadra squadra) {
+        for (int i = 0; i < squadre.size(); i++) {
+            if (squadre.contains(squadra.getNome()))
                 ;
-            else
+            else {
                 squadre.add(squadra.getNome());
-        }
+                int puntiSquadra = n.nextInt(90);
+                classifica.put(squadra.getNome(), n.nextInt(90));
 
-        int puntiSquadra = n.nextInt(90);
-        for (int i = 0; i < punti.size(); i++)
-        {
-            if (puntiSquadra > punti.get(i)) {
-                punti.get(size()) + 1 = punti.size();
-
-                punti.get(i) = puntiSquadra;
             }
         }
+    }
+
+    public static void ordinaClassifica() {
+            //.........................
+        }
+
+
+
 
     }
 
-    //private static ArrayList[][] classifica;
 
 
 
 
 
 
-}
+
+
+
